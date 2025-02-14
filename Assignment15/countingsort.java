54072b93cad68f328bf5bfac8a0da9452d3c3a77
Krishna gopal singh <krishnagopalsingh587@gmail.com>
2025-02-14 23:25:29 +0530

    [Krishna Gopal Singh] Add. Assignment15 commited

---
+++ Assignment15/countingsort.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class countingsort {
+
+    public static int[] countSort(int[] arr){
+        
+        int max = -1;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max , arr[i]);
+        }
+
+        int[] countarray = new int[max+1];
+        
+        for (int i = 0; i < arr.length; i++) {
+            countarray[arr[i]]++;
+        }
+
+        for (int i = 1; i <= max; i++) {
+            countarray[i] += countarray[i-1];
+        }
+
+        int[] output = new int[arr.length];
+
+        for (int i = arr.length-1; i >=0 ; i--) {
+            output[countarray[arr[i]]-1] = arr[i];
+            countarray[arr[i]]--;
+        }
+
+        return output;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size = sc.nextInt();
+        int[] studentAges = new int[size];
+        System.out.println("Enter the students ages between 10 - 18");
+        for (int i = 0; i < studentAges.length; i++) {
+            studentAges[i] = sc.nextInt();
+        }
+        
+
+        System.out.println("Student ages Before Sorting:- " + Arrays.toString(studentAges));
+
+        System.out.println("Student ages After Sorting:- " + Arrays.toString(countSort(studentAges)));
+        sc.close();
+    }
+}
